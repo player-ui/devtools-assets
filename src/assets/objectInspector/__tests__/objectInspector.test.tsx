fcf81f6faf2c35f48aa88df990e9783abfe0e0f9
mercillo <marlon_ercillo@intuit.com>
2024-02-27 22:14:13 -0800

    added objectInspecto

--- src/assets/objectInspector/__tests__/objectInspector.test.tsx
+++ src/assets/objectInspector/__tests__/objectInspector.test.tsx
@@ -1,4 +1,29 @@
 import React from 'react';
 import { render } from 'react-json-reconciler'
 import { ObjectInspector } from '../../../components/src/index';
+import { test, expect } from "vitest";
 
+const expectedBasicObjectInspector = {
+    id: "root",
+    type: "objectInsecptor",
+    label: { asset: { id: "label", type: "text", value: "Label" } },    
+  };
+  
+  const data = {
+    "foo":"bar"
+  }
+  
+  test("works with JSX", async () => {
+    const element = (
+      <ObjectInspector data={data}>
+        <ObjectInspector.Label>
+          <Text>Label</Text>
+          </ObjectInspector.Label>
+      </ObjectInspector>
+    );
+  
+    expect((await render(element)).jsonValue).toStrictEqual(
+        expectedBasicObjectInspector
+    );
+  
+  });
