4c6edeea48c2627ef66800b185aad4d98b5aac7e
mercillo <marlon_ercillo@intuit.com>
2024-03-04 09:16:18 -0800

    merge

---
+++ assets/action/src/dsl/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "@player-ui/text";
+import { AssetPropsWithChildren, Asset, createSlot } from "@player-tools/dsl";
+import { ObjectInspectorAsset } from "../types";
+
+export const ObjectInspector = (
+  props: AssetPropsWithChildren<ObjectInspectorAsset>
+) => <Asset type="object-inspector" {...props} />;
+
+ObjectInspector.Label = createSlot({
+  wrapInAsset: true,
+  name: "label",
+  TextComp: Text,
+});
