136a3c7be091dd9a2e0e34cf7a4db254a28db06a
rcampos2 <rafael_campos@intuit.com>
2024-03-25 10:12:53 -0400

    chore: asset generator

---
+++ plopfile.mjs
@@ -0,0 +1,42 @@
+import fs from "fs";
+import path from "path";
+
+export default function (plop) {
+  plop.setActionType("renameFiles", function (answers) {
+    const { assetName } = answers;
+    const basePath = path.resolve(process.cwd(), assetName);
+    fs.renameSync(
+      path.join(basePath, "README.hbs"),
+      path.join(basePath, "README")
+    );
+    fs.renameSync(
+      path.join(basePath, "BUILD.hbs"),
+      path.join(basePath, "BUILD")
+    );
+    return `${assetName}/README and ${assetName}/BUILD have been renamed`;
+  });
+
+  plop.setGenerator("asset", {
+    description: "Create a new asset",
+    prompts: [
+      {
+        type: "input",
+        name: "assetName",
+        message: "Asset name:",
+      },
+    ],
+    actions: [
+      {
+        type: "addMany",
+        destination: "./{{assetName}}",
+        base: "./template",
+        templateFiles: "./template/**/*",
+        globOptions: { dot: true },
+        stripExtension: true,
+      },
+      {
+        type: "renameFiles",
+      },
+    ],
+  });
+}
