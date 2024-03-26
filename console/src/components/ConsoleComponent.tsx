c72b3c6fecd6fe31b4f4c6e179211300a6017dc4
lexfm <lexfimbres@gmail.com>
2024-03-26 16:28:11 -0700

    Rearranged properties on console component

--- console/src/components/ConsoleComponent.tsx
+++ console/src/components/ConsoleComponent.tsx
@@ -6,33 +6,20 @@ import {
   UnorderedList,
   ListItem,
   FormErrorMessage,
+  Flex,
 } from "@chakra-ui/react";
 import { TransformedConsole } from "../types";
+import { expression as e } from "@player-tools/dsl";
 import { useInputAssetProps } from "@devtools-ui/input";
-
-// const evaluateExpression = e`publish(${INTERACTIONS.EVALUATE_EXPRESSION}, ${bindings.expression})`;
-
-const useConsoleAssetProps = (props: TransformedConsole) => {
-  const inputProps = useInputAssetProps({
-    ...props,
-    binding: props.expression,
-    type: "input",
-  });
-
-  const onSubmit: React.FocusEventHandler<HTMLInputElement> = (e) => {
-    // Logic to get the result of the expression and add it to the History
-  };
-
-  return {
-    inputProps,
-    onSubmit,
-  };
-};
 
 const useConsoleProps = (props: TransformedConsole) => {
   return {
-    inputProps: useConsoleAssetProps(props),
     ...props,
+    inputProps: useInputAssetProps({
+      ...props,
+      binding: props.expression,
+      type: "input",
+    }),
     value: props.expression,
   } as const;
 };
@@ -40,26 +27,28 @@ const useConsoleProps = (props: TransformedConsole) => {
 export const ConsoleComponent = (props: TransformedConsole) => {
   const { evaluations, inputProps, validation } = useConsoleProps(props);
 
+  const onSubmit: React.FocusEventHandler<HTMLInputElement> = (event) => {
+    // Logic to get the result of the expression and add it to the History
+    const evaluateExpression = e`publish(evaluate-expression, ${inputProps.value})`;
+  };
+
   return (
-    <>
+    <Flex direction="column">
       <UnorderedList>
         {evaluations &&
           evaluations.map(({ expression, result, outcome }, idx) => (
             <ListItem key={`console-item-${idx}`}>
-              {`${expression} ${result}`}
+              {`${expression} - ${result}`}
             </ListItem>
           ))}
       </UnorderedList>
-      <FormControl
-        isInvalid={Boolean(validation)}
-        onSubmit={inputProps.onSubmit}
-      >
+      <FormControl isInvalid={Boolean(validation)} onSubmit={onSubmit}>
         <Input {...inputProps} />
         <Button type="submit">Evaluate</Button>
         {validation && (
           <FormErrorMessage>{validation.message}</FormErrorMessage>
         )}
       </FormControl>
-    </>
+    </Flex>
   );
 };
