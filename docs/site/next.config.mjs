257a73708d568b7f5132bd200caecaee2c95c1a7
lexfm <lexfimbres@gmail.com>
2024-03-28 13:58:23 -0700

    First site app setup for docs

---
+++ docs/site/next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+import withMDX from '@next/mdx'
+
+const nextConfig = {
+    // Configure `pageExtensions` to include MDX files
+    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+    // Optionally, add any other Next.js config below
+}
+
+export default withMDX(nextConfig);
