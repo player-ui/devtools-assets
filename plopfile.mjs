f20ad7664fb9809e1ae75ead957e6ba666068c65
lexfm <lexfimbres@gmail.com>
2024-04-25 13:54:54 -0700

    Name kebab-casing and general convention

--- plopfile.mjs
+++ plopfile.mjs
@@ -4,7 +4,7 @@ import path from "path";
 export default function (plop) {
   plop.setActionType("renameFiles", function (answers) {
     const { assetName } = answers;
-    const basePath = path.resolve(process.cwd(), assetName);
+    const basePath = path.resolve(process.cwd(), toKebabCase(assetName));
     fs.renameSync(
       path.join(basePath, "BUILD.hbs"),
       path.join(basePath, "BUILD")
@@ -15,8 +15,8 @@ export default function (plop) {
   plop.setActionType("renameStorybookFiles", function (answers) {
     const { assetName } = answers;
 
-    const pascalCaseName = assetName.replace(/(^\w|-\w)/g, (text) => {
-      return text.replace(/-/, "").toUpperCase();
+    const pascalCaseName = assetName.replace(/(^\w|-\w|\s+\w)/g, (text) => {
+      return text.replace(/(-|\s)/, "").toUpperCase();
     });
 
     const sbTemplatesBasePath = path.resolve(process.cwd(), './docs/storybook/src/assets/');
@@ -34,6 +34,8 @@ export default function (plop) {
     return `${sbTemplatesBasePath}/template.stories.tsx.hbs and ${sbTemplatesBasePath}/template.stories.tsx.hbs have been renamed`;
   })
 
+  plop.setHelper("kebabCase", toKebabCase)
+
   plop.setGenerator("asset", {
     description: "Create a new asset",
     prompts: [
@@ -46,7 +48,7 @@ export default function (plop) {
     actions: [
       {
         type: "addMany",
-        destination: "./{{assetName}}",
+        destination: "./{{kebabCase assetName}}",
         base: "./asset-template",
         templateFiles: "./asset-template/**/*",
         globOptions: { dot: true },
@@ -58,6 +60,14 @@ export default function (plop) {
       ...Object.values(extendedActions)
     ],
   });
+
+  function toKebabCase(text) {
+    return text
+    .replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2')  // getting all lowercase letters that are near to uppercase letters
+    .replace(/[\s_]+/g, '-')                           // replacing all spaces and low dashes
+    .replace(/^-/, '')                              // remove leading - if any
+    .toLowerCase();                                    // Converting all to lowercase
+  }
 }
 
 const extendedActions = {
@@ -70,7 +80,7 @@ const extendedActions = {
   },
   storyBookAssetFlowTemplate: {
     type: "add",
-    path: "./docs/storybook/src/flows/{{assetName}}/basic.tsx",
+    path: "./docs/storybook/src/flows/{{kebabCase assetName}}/basic.tsx",
     templateFile: "./docs/storybook/src/story-templates/flows/basic.tsx.hbs",
   },
   renameStorybookFiles: {
@@ -106,37 +116,37 @@ const extendedActions = {
     type: 'append',
     path: './.bazelignore',
     pattern: /(.|\n)+(.*node_modules)/,
-    template: '{{assetName}}/node_modules',
+    template: '{{kebabCase assetName}}/node_modules',
   },
   pnpmWorkspace: {
     type: 'append',
     path: './pnpm-workspace.yaml',
     pattern: /(.|\n)+(.[\w|"])/,
-    template: '  - "{{assetName}}"',
+    template: '  - "{{kebabCase assetName}}"',
   },
   pluginReadme: {
     type: 'append',
     path: './plugin/README.md',
     pattern: /(.|\n)+(-\s.*)/,
-    template: '- {{assetName}}',
+    template: '- {{kebabCase assetName}}',
   },
   pluginPackageJson: {
     type: 'modify',
     path: './plugin/package.json',
     pattern: /"$/m,
-    template: '",\n    "@devtools-ui/{{assetName}}": "workspace:*"',
+    template: '",\n    "@devtools-ui/{{kebabCase assetName}}": "workspace:*"',
   },
   pluginBazelBuild: {
     type: 'append',
     path: './plugin/BUILD',
     pattern: /(.|\n)+(.@dev.*)/,
-    template: '        ":node_modules/@devtools-ui/{{assetName}}",',
+    template: '        ":node_modules/@devtools-ui/{{kebabCase assetName}}",',
   },
   pluginSrcIndexImportAsset: {
     type: 'append',
     path: './plugin/src/index.ts',
     pattern: /(.|\n)+(import.*from "@.*)/,
-    template: 'import { {{pascalCase assetName}}Asset, {{pascalCase assetName}} } from "@devtools-ui/{{assetName}}";',
+    template: 'import { {{pascalCase assetName}}Asset, {{pascalCase assetName}} } from "@devtools-ui/{{kebabCase assetName}}";',
   },
   pluginSrcIndexExportAsset: {
     type: 'append',
@@ -154,7 +164,7 @@ const extendedActions = {
     type: 'append',
     path: './plugin/src/plugins/AssetsRegistryPlugin.tsx',
     pattern: /(.|\n)+@dev.+/,
-    template: 'import { {{pascalCase assetName}}Asset, {{pascalCase assetName}}Component } from "@devtools-ui/{{assetName}}";',
+    template: 'import { {{pascalCase assetName}}Asset, {{pascalCase assetName}}Component } from "@devtools-ui/{{kebabCase assetName}}";',
   },
   pluginSrcAssetRegistryAssetInterfaceExport: {
     type: 'modify',
@@ -166,18 +176,18 @@ const extendedActions = {
     type: 'modify',
     path: './plugin/src/plugins/AssetsRegistryPlugin.tsx',
     pattern: /(?=\s+]\))/,
-    template: '\n        ["{{assetName}}", {{pascalCase assetName}}Component],',
+    template: '\n        ["{{kebabCase assetName}}", {{pascalCase assetName}}Component],',
   },
   pluginSrcTransformFunctionImport: {
     type: 'append',
     path: './plugin/src/plugins/TransformPlugin.ts',
     pattern: /(.|\n)+@dev.+/,
-    template: 'import { {{camelCase assetName}}Transform } from "@devtools-ui/{{assetName}}";',
+    template: 'import { {{camelCase assetName}}Transform } from "@devtools-ui/{{kebabCase assetName}}";',
   },
   pluginSrcTransformFunctionRegistry: {
     type: 'modify',
     path: './plugin/src/plugins/TransformPlugin.ts',
     pattern: /(?=\s+])/,
-    template: '\n        [{ type: "{{assetName}}" }, {{camelCase assetName}}Transform],',
+    template: '\n        [{ type: "{{kebabCase assetName}}" }, {{camelCase assetName}}Transform],',
   },
 }
