c7d183a46019f70fa30a147aef27894eda090ccb
marky ercillo <marlon_ercillo@intuit.com>
2024-05-15 19:49:21 -0700

    WIP; pnpm next build working

--- docs/site/components/Context.tsx
+++ docs/site/components/Context.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const DEFAULT_CONTEXT = {
   bannerExpanded: true,
@@ -8,7 +8,7 @@ export const DEFAULT_CONTEXT = {
 export const AppContext = React.createContext(DEFAULT_CONTEXT);
 
 export const Context = (props: React.PropsWithChildren<unknown>) => {
-  const [state, setState] = React.useState(DEFAULT_CONTEXT);
+  const [state, setState] = useState(DEFAULT_CONTEXT);
 
   const withHandlers = React.useMemo(() => {
     return {
