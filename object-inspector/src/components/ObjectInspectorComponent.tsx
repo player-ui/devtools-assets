118f64d5fc5d1b3345be9ea498a412eb68cbb3be
lexfm <lexfimbres@gmail.com>
2024-05-20 14:44:05 -0700

    Layout with filter pt 1

--- object-inspector/src/components/ObjectInspectorComponent.tsx
+++ object-inspector/src/components/ObjectInspectorComponent.tsx
@@ -1,22 +1,26 @@
-import React from "react";
-import { Text } from "@chakra-ui/react";
+import React, {useState} from "react";
+import { Text, Input } from "@chakra-ui/react";
 import { ObjectInspector as ObjectorInspectorDS } from "@devtools-ds/object-inspector";
 import { ReactAsset } from "@player-ui/react";
 import { ObjectInspectorAsset } from "../types";
 
 export const ObjectInspectorComponent = (props: ObjectInspectorAsset) => {
   const { data, label, id } = props;
+  const [filterCriteria, setFilterCriteria] = useState('')
 
   return (
     <>
       {label && <ReactAsset {...label} />}
       {data ? (
-        <ObjectorInspectorDS
-          data={data}
-          includePrototypes={false}
-          expandLevel={7}
-          id={id}
-        />
+        <>
+          <Input placeholder="Filter result..." value={filterCriteria} onChange={(e) => {setFilterCriteria(e.target.value)}}/>
+          <ObjectorInspectorDS
+            data={data}
+            includePrototypes={false}
+            expandLevel={7}
+            id={id}
+          />
+        </>
       ) : (
         <Text>No data available</Text>
       )}
