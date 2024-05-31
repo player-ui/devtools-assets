a395d17e6d9a2cc08772c77ad5640d53224c6a7d
lexfm <lexfimbres@gmail.com>
2024-05-31 16:25:08 -0700

    Changed style for drkMode read

--- object-inspector/src/components/ObjectInspectorComponent.tsx
+++ object-inspector/src/components/ObjectInspectorComponent.tsx
@@ -59,7 +59,7 @@ const FilterResults = (props: ObjectInspectorAsset) => {
       />
       <div
         style={{
-          backgroundColor: "#e0e0e0",
+          border: "1px solid #e0e0e0",
           borderRadius: "8px",
           padding: "8px",
         }}
