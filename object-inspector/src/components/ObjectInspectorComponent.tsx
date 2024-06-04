2e2151ef08162a35111684245f29581b0c176643
lexfm <lexfimbres@gmail.com>
2024-06-04 16:44:34 -0700

    UX improvements with filter component

--- object-inspector/src/components/ObjectInspectorComponent.tsx
+++ object-inspector/src/components/ObjectInspectorComponent.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
-import { Text, Input, background } from "@chakra-ui/react";
+import { Text, Input } from "@chakra-ui/react";
 import { ObjectInspector as ObjectorInspectorDS } from "@devtools-ds/object-inspector";
 import { ReactAsset } from "@player-ui/react";
-import { DataModel } from "@player-ui/types";
+import { Flow } from "@player-ui/types";
 import { ObjectInspectorAsset } from "../types";
 
 const FilterResults = (props: ObjectInspectorAsset) => {
   const { data, id } = props;
 
   const [filterCriteria, setFilterCriteria] = useState("");
-  const [resultData, setResultData] = useState("Path result will display here");
+  const [resultData, setResultData] = useState(data);
 
   const isObject = (value: any): boolean => {
     return (
@@ -20,14 +20,13 @@ const FilterResults = (props: ObjectInspectorAsset) => {
     );
   };
 
-  const getPathvalue = (object: DataModel, path: string) => {
+  const getPathvalue = (object: Flow, path: string) => {
     const keys = path.split(".");
     let result: any = object;
     for (const key of keys) {
       const arrayIndex = key.search(/\[\d+\]$/);
 
-      if (!result[key] && arrayIndex === -1)
-        return "No result for the given path";
+      if (!result[key] && arrayIndex === -1) return 0;
 
       // If key has a numeric index, e.g. for Multi-copy and/or array values.
       if (arrayIndex > -1) {
@@ -35,7 +34,7 @@ const FilterResults = (props: ObjectInspectorAsset) => {
         const subIndexMatch = key.match(/(?<=\[)\d+(?=\])/);
 
         result = result[subkey][subIndexMatch ? subIndexMatch[0] : 0];
-        if (!result) return "No result for the given index";
+        if (!result) return -1;
       } else {
         result = result[key];
       }
@@ -43,11 +42,21 @@ const FilterResults = (props: ObjectInspectorAsset) => {
     return result;
   };
 
+  const getResultTag = (result: any) => {
+    if (result === -1) {
+      return "No result for the given index";
+    } else if (result === 0) {
+      return "No result for the given path";
+    } else if (!isObject(result)) {
+      return result;
+    }
+  };
+
   const onChangeHandler: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     setFilterCriteria(e.target.value);
 
-    const result = getPathvalue(data as DataModel, e.target.value);
-    setResultData(result);
+    const result = getPathvalue(data as Flow, e.target.value);
+    setResultData(e.target.value.length ? result : data);
   };
 
   return (
@@ -64,16 +73,15 @@ const FilterResults = (props: ObjectInspectorAsset) => {
           padding: "8px",
         }}
       >
+        <Text style={{ padding: "0 16px" }}>{getResultTag(resultData)}</Text>
         {isObject(resultData) ? (
           <ObjectorInspectorDS
-            data={resultData}
+            data={resultData as Flow}
             includePrototypes={false}
             expandLevel={3}
             id={`filter-${id}`}
           />
-        ) : (
-          <Text style={{ padding: "0 16px" }}>{resultData}</Text>
-        )}
+        ) : null}
       </div>
     </>
   );
@@ -87,13 +95,16 @@ export const ObjectInspectorComponent = (props: ObjectInspectorAsset) => {
       {label && <ReactAsset {...label} />}
       {data ? (
         <>
-          {filter && <FilterResults {...props} style={{ margin: "16px" }} />}
-          <ObjectorInspectorDS
-            data={data}
-            includePrototypes={false}
-            expandLevel={7}
-            id={id}
-          />
+          {filter ? (
+            <FilterResults {...props} style={{ margin: "16px" }} />
+          ) : (
+            <ObjectorInspectorDS
+              data={data}
+              includePrototypes={false}
+              expandLevel={7}
+              id={id}
+            />
+          )}
         </>
       ) : (
         <Text>No data available</Text>
